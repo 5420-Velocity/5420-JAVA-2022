9dc45a941865cc078e71a66f872340999c1ee5f3
jfogg3 <jfogggaming@gmail.com>
2022-02-26 02:23:36 -0500

    Update AutoPixyAlign.java

--- 5420-JAVA-2022-Imported/src/main/java/frc/robot/Commands/AutoPixyAlign.java
+++ 5420-JAVA-2022-Imported/src/main/java/frc/robot/Commands/AutoPixyAlign.java
@@ -4,29 +4,96 @@
 
 package frc.robot.Commands;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.Subsystems.Drivetrain;
 
 public class AutoPixyAlign extends CommandBase {
-  /** Creates a new AutoPixyAlign. */
-  public AutoPixyAlign() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+  
+    private Drivetrain driveTrain;
+    private AtomicBoolean m_lock;
+    private Joystick m_joystick;
+    private boolean finishable;
+    private boolean isFinished;
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+    private PIDController turnPidController = new PIDController(0.03, 0, 0);
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+    private NetworkTableEntry hasTarget = SmartDashboard.getEntry("has target");
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+    private int x;
+    private int y;
+    private int r;
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+
+    @Override
+    public void initialize() {
+        this.isFinished = false;
+        if(m_lock != null){
+            this.m_lock.set(true);
+        }
+    }
+
+    @Override
+     public void execute() {
+
+        final var xSpeed = -m_joystick.getRawAxis(x) * Constants.DriveTrainConstants.kMaxSpeed;
+        // Determine rotation speed for y-axis.
+        final var ySpeed = -m_joystick.getRawAxis(y) * Constants.DriveTrainConstants.kMaxSpeed;
+        final var rotSpeed = -m_joystick.getRawAxis(r) * Constants.DriveTrainConstants.kMaxSpeed;
+    
+        if (driveTrain.pixyAlgo.getPixyBest() != null) {
+            hasTarget.setBoolean(true);
+            // turn to ball
+            double output = turnPidController.calculate(driveTrain.pixyAlgo.getPixyBest().getX(), 150);
+            driveTrain.CanDrive(true);
+            driveTrain.drive(getCurve(xSpeed), getCurve(ySpeed), output, false);
+        }
+        else{
+            // turn to find ball
+            hasTarget.setBoolean(false);
+
+            if ((Math.abs(xSpeed) < Constants.ControllerConstants.NoInputTolerance)
+                    && (Math.abs(ySpeed) < Constants.ControllerConstants.NoInputTolerance)
+                    && (Math.abs(rotSpeed) < Constants.ControllerConstants.NoInputTolerance)) {
+                driveTrain.CanDrive(false);
+            }
+            else{
+                driveTrain.CanDrive(true);
+                driveTrain.drive(getCurve(xSpeed), getCurve(ySpeed), getCurve(rotSpeed), driveTrain.IsFieldRelative());
+            }
+        }
+    }
+
+    public static double getCurve(double input) {
+        double sign = Math.signum(input);
+    
+        double value = Math.abs(input);
+        value = Math.pow(value, 2);
+        value += 0.02;
+    
+        return sign * value;
+    }
+    
+    @Override
+    public void end(boolean interrupted) {
+        if(m_lock != null){
+            this.m_lock.set(false);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        if(finishable){
+            return this.isFinished;
+        }
+        else{
+            return false;
+        }
+    }
 }
